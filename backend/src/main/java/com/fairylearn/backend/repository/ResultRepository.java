17b4e4461516dfb1611282488fbdb933a9b43894
yonngjoone <youngjoone@gmail.com>
2025-09-01 18:51:46 +0900

    Feat: Re-add backend health check to frontend Home page

--- backend/src/main/java/com/fairylearn/backend/repository/ResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fairylearn.backend.repository;
-
-import com.fairylearn.backend.entity.ResultEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepository extends JpaRepository<ResultEntity, Long> {
-}
